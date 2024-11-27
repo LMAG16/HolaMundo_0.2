d15514d0aaf2ab7b0e33ced4b903e36a8efe93b2
Lindsay <lmijhalag2003@gmail.com>
2024-11-26 22:28:18 -0600

    Subir Hola Mundo

---
+++ HolaMundo para Repositorio/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼");
+Console.WriteLine("    HOLA MUNDO    ");
+Console.WriteLine("☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼");
